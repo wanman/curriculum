baeca0f785183624448a8a57cf49969a35c475f1
sng <sng7ca@gmail.com>
2017-06-20 11:31:13 +0300

    normal cat

--- tests/cat.c
+++ tests/cat.c
@@ -1,19 +1,18 @@
-#include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-#undef BUFSIZ
-#define BUFSIZ		4096
-
-#define STDIN		0
+#define STDIN		0	/* standart streams */
 #define STDOUT		1
 #define STDERR		2
 
-/* errors */
-#define NO_ERR		0
-#define WRITE_ERR	(-1)
-#define READ_ERR	(-2)
+#define NO_ERR		0	/* error constants */
+#define READ_ERR	(-1)
+#define WRITE_ERR	(-2)
 #define OPEN_ERR	(-3)
+
+#undef BUFSIZ
+#define BUFSIZ		(1 << 12)	/* better to be divident of block */
 
 int cat(int fd)
 {
@@ -23,23 +22,29 @@ int cat(int fd)
 	while ((bc = read(fd, buf, BUFSIZ)) > 0)
 		if (write(STDOUT, buf, bc) != bc)
 			return WRITE_ERR;
-
 	return bc ? READ_ERR : NO_ERR;
 }
 
 int main(int argc, char *argv[])
 {
-	int fd;
+	int fd, res;
 
-	if (argc == 1)
+	if (argc == 1) {
 		cat(STDIN);
-	else {
+			/* check err here */
+	} else {
 		while (--argc) {
 			fd = open(*++argv, O_RDONLY);
+			if (fd == -1) {
+				write(STDERR, "open err\n", 9);
+				exit(1);
+			}
 			cat(fd);
+				/* check err here */
 			close(fd);
 		}
 	}
 
 	exit(0);
 }
+		
